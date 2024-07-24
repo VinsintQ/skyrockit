f1b82ebb664d3c4b420b93611cfd332286ac1563
Mueen <hejiy2002@gmail.com>
2024-07-24 16:17:11 +0300

    popo

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
-//CONTROLLERS
-const applicationsCtrl = require("./controllers/application.js");
+// IMPORT PATH AT THE TOP OF SERVER
+const path = require("path");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+// CONTROLLERS
+const applicationsCtrl = require("./controllers/applications.js");
 const authController = require("./controllers/auth.js");
-const passUserView = require("./middleware/pass-user-to-view.js");
-const isSignedIn = require("./middleware/is-signed-on.js");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
 
@@ -30,16 +33,22 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(passUserView);
+
+app.use(passUserToView);
+// LINK TO PUBLIC DIRECTORY
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    user: req.session.user,
-  });
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    res.render("index.ejs");
+  }
 });
 
 app.use("/auth", authController);
 app.use(isSignedIn);
 app.use("/users/:userId/applications", applicationsCtrl);
+
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
